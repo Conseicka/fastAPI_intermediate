6d4b255c0f773537ccb76a3473b30e616f76ef7c
conseicka <>
2022-01-12 18:29:04 -0600

    File y UploadFile

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,7 @@ from pydantic import EmailStr
 #FastAPI
 from fastapi import status
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 #uvn helloWorld:app --reload
 app = FastAPI()
@@ -159,3 +159,16 @@ def contact(
     ads: Optional [str] = Cookie(default = None),
 ):
     return user_agent
+
+
+@app.post(
+    path = "/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename":image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read()) / 1027, ndigits = 2)
+    }
